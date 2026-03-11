19109a339127291785cf9642e3b94b9f7a4d19ae
Shehryar <sk2200608@gmail.com>
2026-03-11 14:35:56 +0500

    merge supervisor

--- app/Http/Controllers/manager_controllers/Supervisorcontroller.php
+++ app/Http/Controllers/manager_controllers/Supervisorcontroller.php
@@ -5,17 +5,27 @@ namespace App\Http\Controllers\manager_controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ManagersAccount;
-
-
+use Illuminate\Support\Facades\Auth;
 
 class Supervisorcontroller extends Controller
 {
-    public function index(){
-          $supervisors = ManagersAccount::where('loginas', 'Supervisor')
-                        ->latest('manager_id')
-                        ->get();
+    public function index()
+    {
+        // Get logged in manager
+        $manager = Auth::guard('manager')->user();
 
-        return view('pages.manager.supervisor.Supervisor',compact('supervisors'));
+        if (!$manager) {
+            return redirect('/login');
+        }
+
+        $managerId = $manager->manager_id;
+
+        // Only supervisors assigned to this manager
+        $supervisors = ManagersAccount::where('loginas', 'Supervisor')
+                        ->where('assigned_manager', $managerId)
+                        ->latest('manager_id')
+                        ->paginate();
+
+        return view('pages.manager.supervisor.Supervisor', compact('supervisors'));
     }
 }
-;
